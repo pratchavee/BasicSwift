ed43b16ef5d6b109a5a2882a9f3db4578320f98a
Joe <pratchavee.amorn@gmail.com>
2019-02-11 10:03:18 +0700

    Variable

---
+++ Variable.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+//Variable and Constant
+//Implicit Variable keu karn prakard tee komnod datatype doi chai value kumnod
+var name1 = "Doramon" // String Type
+var number = 123 //Integer Type
+var number2 = 3.1416 //Double Type
+var status1 = true  //Bool Type
+
+//Explcit Variable karn prakarn tee kamnod DataType aenglei//
+var name2: String   = "Nobita"
+var number3: Int = 1234
+var number4: Double = 123.45
+var status2: Bool = false
+
+
+
+
