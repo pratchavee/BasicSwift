fd2a49691151690f75bcc71be9cfdeeb48bcac4c
Joe <pratchavee.amorn@gmail.com>
2019-02-11 10:13:34 +0700

    Operate

--- Variable.playground/Contents.swift
+++ Variable.playground/Contents.swift
@@ -3,7 +3,7 @@ import UIKit
 //Variable and Constant
 //Implicit Variable keu karn prakard tee komnod datatype doi chai value kumnod
 var name1 = "Doramon" // String Type
-var number = 123 //Integer Type
+var number1 = 123 //Integer Type
 var number2 = 3.1416 //Double Type
 var status1 = true  //Bool Type
 
@@ -12,6 +12,16 @@ var name2: String   = "Nobita"
 var number3: Int = 1234
 var number4: Double = 123.45
 var status2: Bool = false
+
+//Constant
+let number5: Int = 1000
+
+number3 = 5000
+//number5 = 5000  //Error
+
+//Operate
+var answer = number1 + 1000 + Int(number2)
+var answer2 = number2 + 0.5 + Double(number1)
 
 
 
